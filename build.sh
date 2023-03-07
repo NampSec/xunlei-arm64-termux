b016ddfdc4dc2339974abcd285338efd774a5007
NampSec <64053970+NampSec@users.noreply.github.com>
2023-03-07 18:06:45 +0800

    add prefix to match termux

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ set -eu
 version=$(cat target/version)
 
 echo $version
-CGO_ENABLED=0 go build -trimpath -ldflags='-s -w -extldflags "-static"' -o bin/xunlei ./
+CGO_ENABLED=0 go build -trimpath -ldflags='-s -w -extldflags "-static"' -o $PREFIX/bin/xunlei ./
 
 if command -v upx >/dev/null; then
-    upx bin/xunlei
+    upx $PREFIX/bin/xunlei
 fi
 
 tgz="xunlei-v${version}.$(uname | tr 'A-Z' 'a-z').$(uname -m).tar.gz"
 echo $tgz
-tar -C bin -zcf bin/$tgz xunlei
-tar -tf bin/$tgz
+tar -C $PREFIX/bin -zcf $PREFIX/bin/$tgz xunlei
+tar -tf $PREFIX/bin/$tgz
